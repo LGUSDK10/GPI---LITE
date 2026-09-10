c1093613f4a8b14ad18c97388c76369f5dfdd580
harley <luiz.gustavo.tec2025@gmail.com>
2026-09-10 16:13:51 -0300

    Update programa.py

--- Sistema Cadastrado/programa.py
+++ Sistema Cadastrado/programa.py
@@ -1,1 +1,28 @@
-print("oi lula")
+import json
+
+while True:
+    print("="*10, "GPI - GERENCIAMENTO DE PROBLEMAS DA INFORMÁTICA", "="*10)
+    print("1- Cadastrar problema\n") #Antônio
+    print("2- Exibir problemas\n")  #Luiz Carlos
+    print("3- Pesquisar problema\n") #Luiz Carlos
+    print("4- Editar problema\n") #Luiz Gustavo
+    print("5- Deletar problema\n") #Luiz Gustavo
+    print("6- Gerar relatório\n") #Enzo
+    print("0- Sair\n") #Antonio
+    opcao = input("Digite a opção desejada: ")
+
+    if opcao == "1":
+
+    elif opcao == "2":
+
+    elif opcao == "3":
+
+    elif opcao == "4":
+
+    elif opcao == "5":
+
+    elif opcao == "6":
+
+    elif opcao == "0":
+
+    else:
