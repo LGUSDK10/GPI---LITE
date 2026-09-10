c83ff8ca94ef2e7120ea0e01bbb64e17723d36d3
gugu <antoniov.tec2025@gmail.com>
2026-09-10 16:58:06 -0300

    Update programa.py

--- Sistema Cadastrado/programa.py
+++ Sistema Cadastrado/programa.py
@@ -1,7 +1,7 @@
 import json
 
 while True:
-    print("="*10, "GPI - GERENCIAMENTO DE PROBLEMAS DA INFORMÁTICA", "="*10)
+    print("="*10, "GPI - GERENCIAMENTO DE PROBLEMAS DA INFORMÁTICA", "="*10, "\n")
     print("1- Cadastrar problema\n") #Antônio
     print("2- Exibir problemas\n")  #Luiz Carlos
     print("3- Pesquisar problema\n") #Luiz Carlos
@@ -12,17 +12,30 @@ while True:
     opcao = input("Digite a opção desejada: ")
 
     if opcao == "1":
+        print("="*69)
+        nome = input("Digite seu problema: ")
+        print("Escolha o local do problema:")
+        print("1- Laboratório de Redes;")
+        print("2- Laboratório de Manutenção;")
+        print("3- Coordenação/Secretária;")
+        print("3- Sala de aula;")
+        local = input("Digite a opção desejada: ")
+    
+        while True:
+            print("Escolha o grau do problema:")
+            print("1- Simples;")
+            print("2- Médio;")
+            print("3- Complexo;")
+            grau = input("Digite a opção desejada: ")
+            if grau > "3" or grau < "1":
+                print("Opção inválida!")
+                continue
+            else:
+                break
+        
+            
 
-    elif opcao == "2":
 
-    elif opcao == "3":
 
-    elif opcao == "4":
 
-    elif opcao == "5":
-
-    elif opcao == "6":
-
-    elif opcao == "0":
-
-    else:
+    
