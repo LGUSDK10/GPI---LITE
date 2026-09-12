5c71730872776af396fd2e6236aae834aa177fcf
antoniovtec2025 <antoniov.tec2025@gmail.com>
2026-09-12 12:06:32 -0300

    Update programa.py

--- Sistema Cadastrado/programa.py
+++ Sistema Cadastrado/programa.py
@@ -1,37 +1,144 @@
 import json
 
+
 while True:
-    print("="*10, "GPI - GERENCIAMENTO DE PROBLEMAS DA INFORMÁTICA", "="*10, "\n")
-    print("1- Cadastrar problema\n") #Antônio
-    print("2- Exibir problemas\n")  #Luiz Carlos
-    print("3- Pesquisar problema\n") #Luiz Carlos
-    print("4- Editar problema\n") #Luiz Gustavo
-    print("5- Deletar problema\n") #Luiz Gustavo
-    print("6- Gerar relatório\n") #Enzo
+    print("="*69)
+    print("="*10, "GPI - GERENCIAMENTO DE PROBLEMAS DA INFORMÁTICA", "="*10)
+    print("="*69, "\n")
+    print("1- Cadastrar problema") #Antônio
+    print("2- Exibir problemas")  #Luiz Carlos
+    print("3- Pesquisar problema") #Luiz Carlos
+    print("4- Editar problema") #Luiz Gustavo
+    print("5- Deletar problema") #Luiz Gustavo
+    print("6- Gerar relatório") #Enzo
     print("0- Sair\n") #Antonio
     opcao = input("Digite a opção desejada: ")
 
+    if opcao == "0":
+        # Saindo do programa
+        print("="*69)
+        print("Saindo do programa...")
+        print("="*69)
+        break
+
     if opcao == "1":
         print("="*69)
-        nome = input("Digite seu problema: ")
-        print("Escolha o local do problema:")
-        print("1- Laboratório de Redes;")
-        print("2- Laboratório de Manutenção;")
-        print("3- Coordenação/Secretária;")
-        print("3- Sala de aula;")
-        local = input("Digite a opção desejada: ")
-    
+        print("Responda as perguntas abaixo para cadastrar o problema:\n")
+
+        # Carregando os registros existentes
+        try:
+            with open("dados.json", "r") as arquivo:
+                lista_problemas = json.load(arquivo)
+
+        except:
+            lista_problemas = []
+
+        # Encontrando o maior ID
+        maior_id = 0
+
+        for problema in lista_problemas:
+            if problema["id"] > maior_id:
+                maior_id = problema["id"]
+
+        id = maior_id + 1
+
+        # Registro do nome, matrícula, tipo, local e grau do problema
+        nome = input("Digite o seu nome: ")
+        print()
+
+        matricula = input("Digite a sua matrícula: ")
+        print()
+
         while True:
-            print("Escolha o grau do problema:")
-            print("1- Simples;")
-            print("2- Médio;")
-            print("3- Complexo;")
-            grau = input("Digite a opção desejada: ")
-            if grau > "3" or grau < "1":
-                print("Opção inválida!")
+            print("Escolha o tipo do problema: \n")
+            print("1- Hardware;")
+            print("2- Software;")
+            print("3- Rede;")
+            print("4- Outro;\n")
+            opcaotipo = input("Digite a opção desejada: ")
+            print()
+
+            if opcaotipo > "4" or opcaotipo < "1":
+                print("Opção inválida!\n")
+                continue
+            elif opcaotipo == "4":
+                tipo = input("Digite o tipo do problema: ")
+                print()
+                break
+            else:
+                if opcaotipo == "1":
+                    tipo = "Hardware"
+                elif opcaotipo == "2":
+                    tipo = "Software"
+                elif opcaotipo == "3":
+                    tipo = "Rede"
+                break
+
+        while True:
+            print("Escolha o local do problema:\n")
+            print("1- Laboratório de Redes;")
+            print("2- Laboratório de Manutenção;")
+            print("3- Coordenação/Secretária;")
+            print("4- Sala de aula;\n") 
+            opcaolocal = input("Digite a opção desejada: ")
+            print()
+
+            if opcaolocal > "4" or opcaolocal < "1":
+                print("Opção inválida!\n")
                 continue
             else:
+                if opcaolocal == "1":
+                    local = "Laboratório de Redes"
+                elif opcaolocal == "2":
+                    local = "Laboratório de Manutenção"
+                elif opcaolocal == "3":
+                    local = "Coordenação/Secretária"
+                elif opcaolocal == "4":
+                    local = "Sala de aula"
                 break
+
+        while True:
+            print("Escolha o grau do problema: \n")
+            print("1- Simples;")
+            print("2- Médio;")
+            print("3- Complexo;\n")
+            opcaograu = input("Digite a opção desejada: ")
+            print()
+
+            if opcaograu > "3" or opcaograu < "1":
+                print("Opção inválida!\n")
+                continue
+            else:
+                if opcaograu == "1":
+                    grau = "Simples"
+                elif opcaograu == "2":
+                    grau = "Médio"
+                elif opcaograu == "3":
+                    grau = "Complexo"
+                break
+
+        # Criando o dicionário de cadastro
+        cadastro = {
+            "id": id,
+            "nome": nome,
+            "matricula": matricula,
+            "tipo": tipo,
+            "local": local,
+            "grau": grau
+        }
+
+        lista_problemas.append(cadastro)
+
+        # Salvando os registros no arquivo JSON
+        with open("dados.json", "w") as arquivo:
+            json.dump(lista_problemas, arquivo, indent=4)
+
+        print("Cadastro realizado com sucesso!\n")
+        print(cadastro)
+
+        
+
+        
         
             
 
